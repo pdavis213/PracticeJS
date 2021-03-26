1ea8f752c156006da244a5dc9d1774664e17240f
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-03-26 17:43:36 -0400

    avg challenge submit

---
+++ ArrayChallenges/Better_than_avg.js
@@ -0,0 +1,20 @@
+function betterThanAverage(arr) {
+    var sum = 0;
+    var avg= 0;
+    // calculate the average
+    for (var i=0;i<arr.length;i++){
+          sum+=arr[i];
+          }
+          avg=sum/arr.length;
+    
+    var count = 0
+    for (var i=0;i<arr.length;i++){
+          if(arr[i]>avg){
+            count++;
+          }
+    }
+    return count;
+}
+    // count how many values are greated than the average
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
